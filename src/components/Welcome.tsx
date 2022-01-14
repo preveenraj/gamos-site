d064859701e57c683db3106974d28e975526e9c1
preveenraj <97preveenraj@gmail.com>
2022-01-14 22:13:16 +0530

    remove heart

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -5,12 +5,12 @@ const Welcome = () => {
   return (
     <div className='relative h-max w-full flex justify-center'>
       <div className={styles.banner}></div>
-      <div className="absolute right-0 h-full w-1/2 hidden lg:flex">
+      {/* <div className="absolute right-0 h-full w-1/2 hidden xl:flex">
         <div className={styles.back}></div>
         <div className={styles.love}></div>
         <div className={styles.love1}></div>
         <div className={styles.love2}></div>
-      </div>
+      </div> */}
     </div>
   );
 };
