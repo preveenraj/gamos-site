536c342136003da662bd97c65d1690aed37adbcc
preveenraj <97preveenraj@gmail.com>
2022-01-30 16:31:18 +0530

    add add to calendar link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,31 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import Icon from "@comp/Icon";
-import { getDatabase, ref, set, onValue } from "firebase/database";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import Icon from '@comp/Icon';
+import { getDatabase, ref, set, onValue } from 'firebase/database';
 
 const Home: NextPage = () => {
   const [icons, setIcons] = useState<any>([]);
   const [likeAnimation, setLikeAnimation] = useState(false);
-  const [likeCount, setLikeCount] = useState("-");
-  const [daysRemaining, setDaysRemaining] = useState("-");
+  const [likeCount, setLikeCount] = useState('-');
+  const [daysRemaining, setDaysRemaining] = useState('-');
   const [audio, setAudio] = useState<HTMLAudioElement>({} as HTMLAudioElement);
   const database = getDatabase();
 
   const initializeAudio = () => {
     const sound = new Audio();
-    sound.src = "/bgm_love.mp3";
+    sound.src = '/bgm_love.mp3';
     // set audio volume
     sound.volume = 0.1;
     setAudio(sound);
-  }
+  };
 
   useEffect(() => {
     initializeAudio();
     addIconsToEvent();
 
-    const starCountRef = ref(database, "likes");
+    const starCountRef = ref(database, 'likes');
     onValue(starCountRef, (snapshot) => {
       const data = snapshot.val();
       setLikeCount(data);
@@ -41,18 +41,17 @@ const Home: NextPage = () => {
 
   const addIconsToEvent = () => {
     const iconsArray = [];
-    const iconCount =
-      window.innerWidth < 500 ? 10 : window.innerWidth < 900 ? 30 : 50;
+    const iconCount = window.innerWidth < 500 ? 10 : window.innerWidth < 900 ? 30 : 50;
     for (let i = 0; i < iconCount; i++) {
       const iconNo = Math.floor(Math.random() * 9) + 1;
       iconsArray.push({
-        key: "key" + i,
+        key: 'key' + i,
         iconNo,
         style: {
           left: `${Math.random() * 100}%`,
           top: `${Math.random() * 100}%`,
           transform: `rotate(${Math.random() * 360}deg)`,
-          animationName: i % 2 === 0 ? "tada" : "swing",
+          animationName: i % 2 === 0 ? 'tada' : 'swing',
           animationDuration: `${Math.random() * 5 + 2}s`,
         },
       });
@@ -63,16 +62,16 @@ const Home: NextPage = () => {
 
   const shareWishes = () => {
     if (typeof likeCount !== 'string') {
-      const isLiked = localStorage.getItem("isLiked");
+      const isLiked = localStorage.getItem('isLiked');
       setLikeAnimation(true);
       window.navigator.vibrate(200);
       setTimeout(() => {
         setLikeAnimation(false);
       }, 1000);
       if (!isLiked) {
-        set(ref(database, "likes"), likeCount + 1);
+        set(ref(database, 'likes'), likeCount + 1);
       }
-      localStorage.setItem("isLiked", "true");
+      localStorage.setItem('isLiked', 'true');
       if (!audio.paused) {
         audio.pause();
       } else {
@@ -85,38 +84,26 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Aswani Weds Vishnu</title>
-        <meta
-          name="description"
-          content="We loved with a love that was more than love."
-        />
+        <meta name="description" content="We loved with a love that was more than love." />
       </Head>
       <div className="page" onDoubleClick={shareWishes}>
         <section className="landing">
+   
           <h1 className="landing__name">Vishnu</h1>
           <h1 className="landing__name">Aswani</h1>
 
-          <div className={`landing__likes ${likeAnimation ? "landing__likes-active" : ""}`}>
+          <div className={`landing__likes ${likeAnimation ? 'landing__likes-active' : ''}`}>
             <div className="landing__likes-count">{likeCount}</div>
             <div className="landing__likes-icon">
-              <Image
-                src="/images/heart.svg"
-                width="36"
-                height="30"
-                alt="likes"
-              />
+              <Image src="/images/heart.svg" width="36" height="30" alt="likes" />
             </div>
           </div>
 
           <div className="landing__actions">
             <button className="landing__action-btn">
               <div>
-                <a href="https://maps.app.goo.gl/h5QvKCrQV9i4FtSY9" className="" target="_blank" rel="noreferrer" >
-                <Image
-                  src="/images/map.svg"
-                  width="32"
-                  height="32"
-                  alt="location"
-                />
+                <a href="https://maps.app.goo.gl/h5QvKCrQV9i4FtSY9" className="" target="_blank" rel="noreferrer">
+                  <Image src="/images/map.svg" width="32" height="32" alt="location" />
                 </a>
               </div>
             </button>
@@ -138,10 +125,21 @@ const Home: NextPage = () => {
               </g>
             </svg>
           </div>
+             <a 
+        className="calendar_banner"
+        rel="noopener noreferrer" target="_blank" href="https://calendar.google.com/event?action=TEMPLATE&tmeid=MDN2Ymk3M3ZtbzhicWQ0YWg5cHRtNGIxb3IgYzBkMGhkOGQ2bjk3cDIzNWJmc3JnNGxlcTRAZw&tmsrc=c0d0hd8d6n97p235bfsrg4leq4%40group.calendar.google.com">
+          <Image 
+          src="/calendar_banner.png" 
+          alt="Add to calendar"
+          width="180"
+          height="140"
+          className='calendar_banner_icon'
+          ></Image>
+        </a>
         </section>
 
-        <section >
-        <iframe className="youtube_embed" src="https://www.youtube.com/embed/gsvtQwAkhF8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <section className="youtube_embed_container">
+          <iframe className="youtube_embed" src="https://www.youtube.com/embed/gsvtQwAkhF8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </section>
 
         <section className="save">
@@ -152,47 +150,36 @@ const Home: NextPage = () => {
           <div className="save__text">
             <div className="save__text-left">Save the date</div>
             <div className="save__text-right">
-              <span className="save__text-place">
-                Near Ernakulam Siva Temple
-              </span>
+              <span className="save__text-place">Near Ernakulam Siva Temple</span>
               <span className="save__text-date">Feb 6, 2022</span>
-              <span className="save__text-event">
-                they&apos;re getting married
-              </span>
+              <span className="save__text-event">they&apos;re getting married</span>
             </div>
           </div>
         </section>
-
+      
         <footer className="footer">
+        <a 
+        className="add_to_calendar"
+        rel="noopener noreferrer" target="_blank" href="https://calendar.google.com/event?action=TEMPLATE&tmeid=MDN2Ymk3M3ZtbzhicWQ0YWg5cHRtNGIxb3IgYzBkMGhkOGQ2bjk3cDIzNWJmc3JnNGxlcTRAZw&tmsrc=c0d0hd8d6n97p235bfsrg4leq4%40group.calendar.google.com">
+          <Image 
+          src="/addtocalendar.png" 
+          alt="Add to calendar"
+          width="76"
+          height="76"
+          className='calendar_icon'
+          ></Image>
+        </a>
           <div className="footer-wishes">
             Best wishes
             <span>-Ajay, Anjali &amp; Preveenraj</span>
           </div>
         </footer>
 
-        <div
-          className={`like-icon__outer ${
-            likeAnimation ? "like-icon__outer--active" : ""
-          }`}
-        >
-          <Image
-            src={"/images/like-heart-outer.svg"}
-            alt="Like Animation"
-            width={714}
-            height={603}
-          />
+        <div className={`like-icon__outer ${likeAnimation ? 'like-icon__outer--active' : ''}`}>
+          <Image src={'/images/like-heart-outer.svg'} alt="Like Animation" width={714} height={603} />
         </div>
-        <div
-          className={`like-icon__inner ${
-            likeAnimation ? "like-icon__inner--active" : ""
-          }`}
-        >
-          <Image
-            src={"/images/like-heart-inner.svg"}
-            alt="Like Animation"
-            width={592}
-            height={468}
-          />
+        <div className={`like-icon__inner ${likeAnimation ? 'like-icon__inner--active' : ''}`}>
+          <Image src={'/images/like-heart-inner.svg'} alt="Like Animation" width={592} height={468} />
         </div>
       </div>
     </>
